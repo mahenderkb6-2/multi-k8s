67f6035a1162aab08328dd4286889a4d5908fd42
mahenderkumarb <mahenderb6365@gmail.com>
2023-04-24 17:17:14 +0530

    added deployment scripts

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Mahender
+            Learn React Mahender:v2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
