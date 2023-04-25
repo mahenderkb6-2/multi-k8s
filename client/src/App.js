4e2da903a466055bff45e09a80415bcd2c83491c
mahenderkumarb <mahenderb6365@gmail.com>
2023-04-25 12:29:26 +0530

    updated header-02

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Mahender:kubernetes
+            Learn React Mahender:kubernetess 
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
@@ -32,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
